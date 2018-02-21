4fac81b15bc8207d9fcf13f4b1aa6a374724032d
Hans Schoenburg <hschoenburg@gmail.com>
2018-02-20 19:43:23 -0800

    WIP

--- spec/helpers/db_helper.js
+++ spec/helpers/db_helper.js
@@ -1,7 +1,9 @@
 var db = require('../../handshake/db')
 
 async function seedJob (opts) {
-  return true
+  const insert = "INSERT INTO jobs (token, qualcode, username, status) VALUES($1, $2, $3, $4) RETURNING *"
+  let result = await db.query(insert, ['blah', 'yup', 'hackerMe', opts.status])
+  return result.rows[0]
 }
 
 module.exports.seedJob = seedJob
--- spec/runner_spec.js
+++ spec/runner_spec.js
@@ -1,10 +1,75 @@
 const subject = require('../runner')
+const qUser = require('../lib/qUser')
+const tokenServer = require('../handshake/token_server')
 
 describe('getJobs()', () => {
+  beforeEach(done => {
+    spyOn(subject, 'runJobs')
+    done()
+  })
   it('returns true when no jobs are present', (done) => {
     subject.getJobs()
     .then(j => {
       expect(j).toBe(true)
+      done()
+    })
+  })
+
+  it('does not call runJobs', (done) => {
+    expect(subject.runJobs).not.toHaveBeenCalled()
+    done()
+  })
+
+  describe('with one pending job in db', () => {
+    var job
+    beforeEach(done => {
+      dbHelper.seedJob({status: 'pending'})
+        .then(j => {
+          job = j
+          done()
+        })
+    })
+
+    it('calls runJob', (done) => {
+      expect(subject.runJob).toHaveBeenCalled
+      done()
+    })
+  })
+})
+
+describe('runJobs()', () => {
+  var job
+  beforeEach(done => {
+    dbHelper.seedJob({status: 'pending'})
+    .then(j => {
+      job = j
+      done()
+    })
+  })
+
+
+  it('calls the token server', (done) => {
+    // with job.username
+    done()
+  })
+
+  it('calls qUser.realUser()', (done) => {
+    // with job.username
+    done()
+  })
+
+  it('calls qUser.fossScore()', (done) => {
+    done()
+  })
+
+  describe('with a not real user', () => {
+    beforeEach(done => {
+      spyOn(qUser, 'realUser').and.returnValue(false)
+      done()
+    })
+
+    it('does not call fossScore', (done) => {
+      expect(subject.fossScore).not.toHaveBeenCalled()
       done()
     })
   })
